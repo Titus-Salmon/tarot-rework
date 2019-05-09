3cc6324d1555d42583529f65ca1f7b19deab0f4e
tsalm0n <tsalm0n@twc.com>
2019-05-09 19:53:22 -0400

    ES6 modularization of deck array

---
+++ js/deckObj.js
@@ -0,0 +1,27 @@
+//begin define cards as objects
+var c0 = {
+    name: 'The Fool',
+    imgSrcUp: '../img/cardimg/00.jpg',
+    imgSrcDn: '../img/cardimg/00r.jpg'
+}
+
+var c1 = {
+    name: 'The Magician',
+    imgSrcUp: '../img/cardimg/01.jpg',
+    imgSrcDn: '../img/cardimg/01r.jpg'
+}
+
+var c2 = {
+    name: 'The High Priestess',
+    imgSrcUp: '../img/cardimg/02.jpg',
+    imgSrcDn: '../img/cardimg/02r.jpg'
+}
+//end define cards as objects
+
+
+let deckArray = [c0, c1, c2];
+
+export default deckArray;
+
+
+
--- js/shuffle.js
+++ js/shuffle.js
@@ -1,54 +1,27 @@
-//begin define cards as objects
-
-var c0 = {
-    name: 'The Fool',
-    imgSrcUp: '../img/cardimg/00.jpg',
-    imgSrcDn: '../img/cardimg/00r.jpg'
-}
-
-var c1 = {
-    name: 'The Magician',
-    imgSrcUp: '../img/cardimg/01.jpg',
-    imgSrcDn: '../img/cardimg/01r.jpg'
-}
-
-var c2 = {
-    name: 'The High Priestess',
-    imgSrcUp: '../img/cardimg/02.jpg',
-    imgSrcDn: '../img/cardimg/02r.jpg'
-}
-
-//end define cards as objects
-
-
-const deck = [
-    c0, c1, c2
-]
+import deckArray from './deckObj';
 
 
 //begin SHUFFLE DECK
-
-function shuffle(deck) {
-    for (let i = deck.length - 1; i > 0; i--) {
+function shuffle(deckArray) {
+    for (let i = deckArray.length - 1; i > 0; i--) {
         const j = Math.floor(Math.random() * (i + 1));
-        [deck[i], deck[j]] = [deck[j], deck[i]];
+        [deckArray[i], deckArray[j]] = [deckArray[j], deckArray[i]];
     }
 }
 
-shuffle(deck);
+shuffle(deckArray);
 
-console.log('shuffled deck==>', deck);
-
+console.log('shuffled deckArray==>', deckArray);
 //end SHUFFLE DECK
 
 
-//begin show shuffled deck
 
-deck.forEach(function (card) {
+
+//begin show shuffled deck
+deckArray.forEach(function (card) {
     var image = document.createElement('img');
     image.src = card.imgSrcUp;
     document.body.appendChild(image);
 });
-
 //end show shuffled deck
 
