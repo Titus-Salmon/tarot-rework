b7ce6ad4a9c4f337dd54fe8c3ffd19cdc7707c39
tsalm0n <tsalm0n@twc.com>
2019-05-10 14:47:10 -0400

    include 'upright/reverse' card display functionality within shuffle function; clear previous cards before next shuffle

--- js/shuffle.js
+++ js/shuffle.js
@@ -1,24 +1,36 @@
 import deckArray from './deckObj';
 
+const shuffleButton = document.getElementById('shuffle');
+const turnButton = document.getElementById('turn');
 
-//begin Durstenfeld shuffle
 function shuffle(deckArray) {
+    //begin Durstenfeld shuffle
     for (let i = deckArray.length - 1; i > 0; i--) {
         const j = Math.floor(Math.random() * (i + 1));
         [deckArray[i], deckArray[j]] = [deckArray[j], deckArray[i]];
     }
+    //end Durstenfeld shuffle
+    console.log('shuffled deckArray==>', deckArray);
+
+    //begin assign images to shuffled cards & allow for upright/reversed cards
+    deckArray.forEach(function (card) {
+        var image = document.createElement('img');
+        Math.random() > .5 ? image.src = card.imgSrcUp : image.src = card.imgSrcDn; //flip coin for up or down card
+        document.body.appendChild(image);
+    });
+    //end assign images to shuffled cards & allow for upright/reversed cards
+    console.log('shuffled deckArray after coin toss==>', deckArray);
 }
-//end Durstenfeld shuffle
 
-
-shuffle(deckArray);
-console.log('shuffled deckArray==>', deckArray);
-
-
-//begin show shuffled deck
-deckArray.forEach(function (card) {
-    var image = document.createElement('img');
-    Math.random() > .5 ? image.src = card.imgSrcUp : image.src = card.imgSrcDn; //flip coin for up or down card
-    document.body.appendChild(image);
+shuffleButton.addEventListener('click', () => {
+    //if there are cards present from a previous shuffle, remove them
+    while (document.body.getElementsByTagName('img').length > 0) {
+        document.body.removeChild(document.body.lastElementChild);
+    }
+    //then do shuffle
+    shuffle(deckArray)
 });
-//end show shuffled deck
+
+turnButton.addEventListener('click', () => {
+    turn();
+});
