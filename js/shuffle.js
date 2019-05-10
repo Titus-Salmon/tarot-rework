c20323046c865e91998242f1610143c9a43ec0ab
tsalm0n <tsalm0n@twc.com>
2019-05-09 21:03:19 -0400

    added 50-50 probability of 'up' or 'down' for each card

--- js/shuffle.js
+++ js/shuffle.js
@@ -1,27 +1,24 @@
 import deckArray from './deckObj';
 
 
-//begin SHUFFLE DECK
+//begin Durstenfeld shuffle
 function shuffle(deckArray) {
     for (let i = deckArray.length - 1; i > 0; i--) {
         const j = Math.floor(Math.random() * (i + 1));
         [deckArray[i], deckArray[j]] = [deckArray[j], deckArray[i]];
     }
 }
+//end Durstenfeld shuffle
+
 
 shuffle(deckArray);
-
 console.log('shuffled deckArray==>', deckArray);
-//end SHUFFLE DECK
-
-
 
 
 //begin show shuffled deck
 deckArray.forEach(function (card) {
     var image = document.createElement('img');
-    image.src = card.imgSrcUp;
+    Math.random() > .5 ? image.src = card.imgSrcUp : image.src = card.imgSrcDn; //flip coin for up or down card
     document.body.appendChild(image);
 });
 //end show shuffled deck
-
