6a8eaaeb3282500dda64680ea9f99b64d8631467
tsalm0n <tsalm0n@twc.com>
2019-05-10 23:31:10 -0400

    added select deck type functionality

--- js/deckObj.js
+++ js/deckObj.js
@@ -4,22 +4,35 @@ var c0 = {
     imgSrcUp: '../img/cardimg/00.jpg',
     imgSrcDn: '../img/cardimg/00r.jpg'
 }
-
 var c1 = {
     name: 'The Magician',
     imgSrcUp: '../img/cardimg/01.jpg',
     imgSrcDn: '../img/cardimg/01r.jpg'
 }
-
 var c2 = {
     name: 'The High Priestess',
     imgSrcUp: '../img/cardimg/02.jpg',
     imgSrcDn: '../img/cardimg/02r.jpg'
 }
+var c3 = {
+    name: 'The Empress',
+    imgSrcUp: '../img/cardimg/03.jpg',
+    imgSrcDn: '../img/cardimg/03r.jpg'
+}
+var c4 = {
+    name: 'The Emperor',
+    imgSrcUp: '../img/cardimg/04.jpg',
+    imgSrcDn: '../img/cardimg/04r.jpg'
+}
+var c5 = {
+    name: 'The Hierophant',
+    imgSrcUp: '../img/cardimg/05.jpg',
+    imgSrcDn: '../img/cardimg/05r.jpg'
+}
 //end define cards as objects
 
 
-let deckArray = [c0, c1, c2];
+let deckArray = [c0, c1, c2, c3, c4, c5];
 
 export default deckArray;
 
--- js/shuffle.js
+++ js/shuffle.js
@@ -1,25 +1,81 @@
-import deckArray from './deckObj';
+import deckArray from './deckObj'; //import entire deck
+
+var minorDeckArray = [];
+
+function createMinor() {    //create Minor Arcana deck from imported entire deck
+    for (let i = 0; i < 3; i++) {
+        minorDeckArray.push(deckArray[i]);
+    }
+}
+createMinor();
+console.log('minorDeckArray===>', minorDeckArray);
+
+
+var majorDeckArray = [];
+
+function createMajor() {    //create Major Arcana deck from imported entire deck
+    for (let i = 3; i < 6; i++) {
+        majorDeckArray.push(deckArray[i]);
+    }
+}
+createMajor();
+console.log('majorDeckArray===>', majorDeckArray);
+
+
+var deckToShuffle = deckArray;  //set default deck to use for shuffle() & turn() to entire deck
+
+/** begin set deckToShuffleArray based on selected option in dropdown*/
+var deckSelectId = document.getElementById('deckSelect');
+var deckSelectValue = deckSelectId.options[deckSelectId.selectedIndex].value;
+console.log('deckSelectValue===>', deckSelectValue);
+
+document.getElementById('deckSelect').addEventListener('change', () => {
+    console.log('selectedDeck==>', document.getElementsByTagName('option')[deckSelectId.selectedIndex].value);
+
+    if (document.getElementsByTagName('option')[deckSelectId.selectedIndex].value !== 'fullDeck') {
+        if (document.getElementsByTagName('option')[deckSelectId.selectedIndex].value == 'majorDeck') {
+            deckToShuffle = majorDeckArray;
+        } else {
+            deckToShuffle = minorDeckArray;
+        }
+    } else {
+        deckToShuffle = deckArray;
+    }
+});
+/** end set deckToShuffleArray based on selected option in dropdown*/
+
 
 const shuffleButton = document.getElementById('shuffle');
 const turnButton = document.getElementById('turn');
 
-function shuffle(deckArray) {
+
+function shuffle(deckToShuffle) {
     //begin Durstenfeld shuffle
-    for (let i = deckArray.length - 1; i > 0; i--) {
+    for (let i = deckToShuffle.length - 1; i > 0; i--) {
         const j = Math.floor(Math.random() * (i + 1));
-        [deckArray[i], deckArray[j]] = [deckArray[j], deckArray[i]];
+        [deckToShuffle[i], deckToShuffle[j]] = [deckToShuffle[j], deckToShuffle[i]];
     }
     //end Durstenfeld shuffle
-    console.log('shuffled deckArray==>', deckArray);
+    console.log('shuffled deckToShuffle==>', deckToShuffle);
 
+    deckToShuffle.forEach(function () {
+        var image = document.createElement('img');
+        image.src = '../img/cardimg/rider-waite-original-back.jpg';
+        image.width = "100";
+        image.height = "171";
+        document.body.appendChild(image);
+    });
+}
+
+function turn() {
     //begin assign images to shuffled cards & allow for upright/reversed cards
-    deckArray.forEach(function (card) {
+    deckToShuffle.forEach(function (card) {
         var image = document.createElement('img');
         Math.random() > .5 ? image.src = card.imgSrcUp : image.src = card.imgSrcDn; //flip coin for up or down card
         document.body.appendChild(image);
     });
     //end assign images to shuffled cards & allow for upright/reversed cards
-    console.log('shuffled deckArray after coin toss==>', deckArray);
+    console.log('shuffled deckToShuffle after coin toss==>', deckToShuffle);
 }
 
 shuffleButton.addEventListener('click', () => {
@@ -28,9 +84,14 @@ shuffleButton.addEventListener('click', () => {
         document.body.removeChild(document.body.lastElementChild);
     }
     //then do shuffle
-    shuffle(deckArray)
+    shuffle(deckToShuffle)
 });
 
 turnButton.addEventListener('click', () => {
+    //if there are cards present from a previous shuffle, remove them
+    while (document.body.getElementsByTagName('img').length > 0) {
+        document.body.removeChild(document.body.lastElementChild);
+    }
+    //then do turn
     turn();
 });
